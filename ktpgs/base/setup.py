4736e69476a73fecc6ab137db03a4b85ffafd00d
noname <samqhrobya@liquidid.net>
2014-01-26 13:02:04 +0900

    modified:   ktpgs/base/setup.py

--- ktpgs/base/setup.py
+++ ktpgs/base/setup.py
@@ -13,7 +13,7 @@ setup(
     name = "ketpegs",
     author = "",
     author_email = "",
-    version = "0.9.23",
+    version = "0.9.29",
     license = "GPL3",
     description = "A thicket and pegs game using the curses",
     long_description = "README",
