204e1e68029258f003370f6437d87b9bfcb35914
noname <samqhrobya@liquidid.net>
2014-01-26 10:12:38 +0900

    new file:   ktpgs/other/common.py

---
+++ ktpgs/other/common.py
@@ -0,0 +1,8 @@
+m12_rule= {
+        'a':'rl2rl5rl4',
+        'b':"rlrl3rl2",
+        'c':"rlrl3rl2rl9rl7rl8",
+        'd':"rl3rl6rlrl9rl7rl8",
+        'e':"rl9rl7rl8rl7rlrl5",
+        'f':"rlrl3rl2rl7rlrl5rl7rlrl5",
+        }
